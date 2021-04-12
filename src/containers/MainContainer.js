a6c525380db0fe2a7dd53ab309ff3329d07aa661
prahal <vprahaladhan@gmail.com>
2021-04-12 17:11:32 +1000

    commit tree 49ea8d26b214dc4f7eb80be8aaa3d7de61716790

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,28 +4,65 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  state = {
+    stocks: [],
+    portfolio: [],
+    type: null
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/stocks')
+      .then(response => response.json())
+      .then(stocks => this.setState({
+        stocks,
+        filteredStocks: stocks
+      }))
+  }
+
+  addStockToPortfolio = stock => this.setState(prevState => {
+    if (!prevState.portfolio.find(portfolioStock => portfolioStock.id === stock.id)) {
+      return { 
+        ...prevState, 
+        portfolio: prevState.portfolio.concat(stock)
+      }  
+    }
+  });
+
+  sellStockFromPortfolio = stock => this.setState(prevState => {
+    const portfolio = prevState.portfolio;
+    const index = portfolio.findIndex(portfolioStock => portfolioStock.id === stock.id);
+    portfolio.splice(index, 1);
+    return { ...prevState, portfolio };  
+  });
+
+  sortStocks = order => {
+    const sortByPrice = (order === 'price');
+    this.setState(prevState => {
+      const newState = { 
+        ...prevState, 
+        stocks: prevState.stocks.sort((a, b) => sortByPrice ? a.price - b.price : a.name.localeCompare(b.name)) 
+      };
+      return newState;
+    });
+  }
+
+  filterStocks = type => this.setState({ type });
 
   render() {
     return (
       <div>
-        <SearchBar/>
-
-          <div className="row">
-            <div className="col-8">
-
-              <StockContainer/>
-
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
+        <SearchBar sortStocks={this.sortStocks} changeFilterType={this.filterStocks} />
+        <div className="row">
+          <div className="col-8">
+            <StockContainer stocks={this.state.stocks} sortStocks={this.addStockToPortfolio} type={this.state.type}/>
           </div>
+          <div className="col-4">
+            <PortfolioContainer portfolio={this.state.portfolio} onClick={this.sellStockFromPortfolio} />
+          </div>
+        </div>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
